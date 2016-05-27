60f8d69bcce4ec3e989f042a4838c871e7c81309
egonmin <egonmin@CN00119199>
2016-05-27 11:00:36 +0800

    Add project files.

---
+++ CheckZigZagSequence/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class ZigZag {
+
+public:
+	int longestZigZag(int sequence[], int size);
+};
+
+int ZigZag::longestZigZag(int sequence[], int size) {
+
+
+	return 0;
+}
+
+int main()
+{
+
+	return 0;
+}
